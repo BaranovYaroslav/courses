9d7edd41a0e9f5a0e98093026c42f118c992ff37
ynbaranov9196 <ynbaranov9196@gmail.com>
2017-07-19 06:25:33 +0300

    Update validation regex

--- src/main/java/constants/ValidationConstants.java
+++ src/main/java/constants/ValidationConstants.java
@@ -10,7 +10,7 @@ public interface ValidationConstants {
     String PASSWORD_REGEX = "(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{5,}$";
     String NAME_REGEX = "^\\pL+[\\pL\\pZ\\pP]{0,}$";
     String EMAIL_REGEX = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-    String UTF8_TRIMMED_STRING_REGEX = "^(?![^@]*@)(?!.*\\s\\s)(?=(?:\\P{L}*\\p{L}){2})\\S.{0,14}\\S$";
+    String UTF8_TRIMMED_STRING_REGEX = "^(?![^@]*@)(?!.*\\s\\s)(?=(?:\\P{L}*\\p{L}){2})\\S.{0,200}\\S$";
     String DOUBLE_REGEX = "^-?\\d+(?:\\.\\d+)?$";
     String POSITIVE_DOUBLE_REGEX = "^\\(?[\\d.]+\\)?$";
     String INTEGER_GREATER_THAN_ZERO_REGEX = "^[1-9][0-9]*$";
